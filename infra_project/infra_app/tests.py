43822949aae5f73033dbb9eb76f6c6fed4c874cf
Anastasiya <nastya.eremenko@mail.ru>
2021-06-29 21:41:26 +0300

    Test action tests v4

--- infra_project/infra_app/tests.py
+++ infra_project/infra_app/tests.py
@@ -12,12 +12,13 @@ class StaticPagesURLTests(TestCase):
         response = self.guest_client.get('/')
         self.assertEqual(response.status_code, HTTPStatus.OK.value)
 
-        response = self.guest_client.get('/second_page/')
+        response = self.guest_client.get('/second/')
         self.assertEqual(response.status_code, HTTPStatus.OK.value)
 
     def test_page_shows_correct_context(self):
         """Проверка контекста страниц."""
         response = self.guest_client.get('/')
         self.assertContains(response, 'У меня получилось!')
-        response = self.guest_client.get('/second_page/')
-        self.assertContains(response, 'А это вторая страница!')
+
+        response = self.guest_client.get('/second/')
+        self.assertContains(response, 'А это вторая страница')
